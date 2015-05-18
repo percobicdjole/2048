e32322253ef76b8230c213de9fb79b07c5a98e6a
vidorge <vidorge@gmail.com>
2015-05-18 16:44:44 +0200

    test curses

--- PP2-Project/main.c
+++ PP2-Project/main.c
@@ -1,5 +1,14 @@
 
+#include <curses.h>
+#include <stdio.h>
 
 int main() {
+
+	initscr();                    /* Start curses mode */
+	printw("Hello World !!!");    /* Print Hello World */
+	refresh();                    /* Print it on to the real screen */
+	getch();                      /* Wait for user input */
+	endwin();                     /* End curses mode */
+
 	return 0;
 }
