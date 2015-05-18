1c109e993b0ee7dabbd975816ea4903085f5c869
Ivan <blazic.ivan@outlook.com>
2015-05-18 16:17:46 +0200

    Prva verzija kôda za logiku
    Funkcije su poboljsane u odnosu na prototip, sada rade efikasnije i
    vracaju broj promena u matrici. Heder je izmenjen, izbaceni su
    nepotrebni tipovi i sada se koristi samo short. Postoji jedna mala
    greska u samom pravilu igre kada se sabiraju brojevi.

---
+++ Logic.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Logic.h"
+
+//Postavljanje SEED-a
+void setSeed()
+{
+	srand(time(NULL));
+}
+
+
+//Random broj u opsegu
+int randomInt(int low,int high)
+{
+	return low + rand() / (RAND_MAX / (high - low + 1) + 1);
+}
+
+//Kreiranje nove matrice
+matrix newMatrix(short size)
+{
+	matrix N;
+	short i;
+	short **M = malloc(size*sizeof(short*));
+	for (i = 0; i < size; i++)
+	{
+		M[i] = calloc(size, sizeof(short));
+	}
+	N.count = 0;
+	N.set = M;
+	N.size = size;
+	N.score = 0;
+	spawnNumber(&N);
+	spawnNumber(&N);
+	return N;
+}
+
+//Ubacuje broja 2 na random izabrano prazno mesto
+void spawnNumber(matrix *M)
+{
+	//Problem sa dimnamickom memorijom
+	short i, j, array_index = -1, matrix_index = 0;
+	short *free_tiles[25];
+	for (i = 0; i < M->size; i++)
+		for (j = 0; j < M->size; j++)
+		{
+			if (M->set[i][j] == 0)
+			{
+				array_index++;
+				free_tiles[array_index] = matrix_index;
+			}
+			matrix_index++;
+		}
+
+	array_index = randomInt(0, array_index);
+	matrix_index = free_tiles[array_index];
+	i = matrix_index / M->size;
+	j = matrix_index % M->size;
+	M->set[i][j] = 2;
+	M->count++;
+}
+
+
+//Pomera jedan red za jedno mesto
+short moveRowStep(matrix *M, short row, short direction)
+{
+	short j, changes = 0;
+	if (direction == RIGHT)
+	{
+		for (j = M->size - 2; j >= 0; j--)
+		{
+			if (M->set[row][j] != 0)
+			{
+				if (M->set[row][j + 1] == 0 || M->set[row][j + 1] == M->set[row][j])
+				{
+					M->set[row][j + 1] += M->set[row][j];
+					M->set[row][j] = 0;
+					M->count--;
+					M->score += M->set[row][j + 1];
+					changes++;
+				}
+			}
+		}
+	}
+	else if (direction == LEFT)
+	{
+		for (j = 1; j < M->size; j++)
+		{
+			if (M->set[row][j] != 0)
+			{
+				if (M->set[row][j - 1] == 0 || M->set[row][j - 1] == M->set[row][j])
+				{
+					M->set[row][j - 1] += M->set[row][j];
+					M->set[row][j] = 0;
+					M->count--;
+					M->score += M->set[row][j - 1];
+					changes++;
+				}
+			}
+		}
+	}
+	return changes;
+}
+
+
+//Pomera jednu kolonu za jedno mesto
+short moveColumnStep(matrix *M, short column, short direction)
+{
+	short i, changes = 0;
+	if (direction == UP)
+	{
+		for (i = 1; i < M->size; i++)
+		{
+			if (M->set[i][column] != 0)
+			{
+				if (M->set[i - 1][column] == 0 || M->set[i - 1][column] == M->set[i][column])
+				{
+					M->set[i - 1][column] += M->set[i][column];
+					M->set[i][column] = 0;
+					M->count--;
+					M->score += M->set[i - 1][column];
+					changes++;
+				}
+			}
+		}
+	}
+	else if (direction == DOWN)
+	{
+		for (i = M->size - 2; i >= 0; i--)
+		{
+			if (M->set[i][column] != 0)
+			{
+				if (M->set[i + 1][column] == 0 || M->set[i + 1][column] == M->set[i][column])
+				{
+					M->set[i + 1][column] += M->set[i][column];
+					M->set[i][column] = 0;
+					M->count--;
+					M->score += M->set[i + 1][column];
+					changes++;
+				}
+			}
+		}
+	}
+	return changes;
+}
+
+
+//Pomera matricu postepeno i vraca broj promena
+short moveStep(matrix M, short direction)
+{
+	short i, j, changes = 0;
+	if (direction == RIGHT || direction == LEFT)
+	{
+		for (i = 0; i < M.size; i++)
+		{
+			changes += moveRowStep(&M, i, direction);
+		}
+	}
+
+	if (direction == UP || direction == DOWN)
+	{
+		for (j= 0; j < M.size; j++)
+		{
+			changes += moveColumnStep(&M, j, direction);
+		}
+	}
+	return changes;
+}
+
+
+//Pomera matricu u jednom potezu
+void move(matrix M, short direction)
+{
+	short changes;
+	//Detektuje promene u matrici
+	changes = moveStep(M, direction);
+	while (changes)
+	{
+		changes = moveStep(M, direction);
+	}
+	if (changes)
+		spawnNumber(&M);
+}
+
+
+//Stampa matricu
+void printMatrix(matrix M)
+{
+	short i, j;
+	for (i = 0; i < M.size; i++)
+	{
+		for (j = 0; j < M.size; j++)
+			printf("%d ", M.set[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+//Stampa red
+void printRow(short *R, short size)
+{
+	short i, j;
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", R[i]);
+	}
+	printf("\n");
+}
+
+
+void main()
+{
+	/*Demonstracija pomeraja iz jednog poteza:
+	Gore, dole, levo desno.
+	*/
+	short i, c;
+	setSeed();
+	matrix M = newMatrix(4);
+	printMatrix(M);
+	for (i = 0; i < 4; i++)
+	{
+		move(M, UP);
+		printMatrix(M);
+		move(M, DOWN);
+		printMatrix(M);
+		move(M, LEFT);
+		printMatrix(M);
+		move(M, RIGHT);
+		printMatrix(M);
+	}
+	system("PAUSE");
+}
---
+++ Logic.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//Zamena za argumente funkcija pomeranja
+#define LEFT 0
+#define RIGHT 1
+#define UP 2
+#define DOWN 3
+
+//Potrebne konstante
+#define TRUE 1
+#define FALSE 0
+
+//Matrica
+typedef struct matrix
+{
+	short **set;//novo ime
+	char size, count;
+	short score;
+}matrix;
+
+
+//Funkcije za testiranje
+void printRow(short *R, short size);
+void printMatrix(matrix M);
+
+
+//Funkcije koje se pozivaju na poèetku igre
+void setSeed();
+matrix newMatrix(short size);
+
+
+//Pomocne funkcije
+void spawnNumber(matrix *M);
+int randomInt(int low, int high);
+
+
+//Funkcije za pomeranje
+short moveStep(matrix M, short direction);
+void move(matrix M, short direction);
